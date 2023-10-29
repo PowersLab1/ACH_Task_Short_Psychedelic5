bd721221fbf85d6bee7d497d2190903c4c01840c
Max Greenwald <maximillian.greenwald@yale.edu>
2023-10-29 15:04:14 -0400

    Updates

---
+++ precache-manifest.fcf0ae6c0b72000de69757c6e44bd2a1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cf0b1c62a20d71d3470ffa1e3d3f308",
+    "url": "/ACH_Task_Short_Psychedelic5/index.html"
+  },
+  {
+    "revision": "3a97c676bd63e32b55df",
+    "url": "/ACH_Task_Short_Psychedelic5/static/css/main.f79609ab.chunk.css"
+  },
+  {
+    "revision": "8a981007626a35d62e17",
+    "url": "/ACH_Task_Short_Psychedelic5/static/js/2.32fe8434.chunk.js"
+  },
+  {
+    "revision": "ee45c4ac1e2cdbe20661beb901b1137f",
+    "url": "/ACH_Task_Short_Psychedelic5/static/js/2.32fe8434.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a97c676bd63e32b55df",
+    "url": "/ACH_Task_Short_Psychedelic5/static/js/main.7e3a400a.chunk.js"
+  },
+  {
+    "revision": "48aa33ff0119403e3081",
+    "url": "/ACH_Task_Short_Psychedelic5/static/js/runtime-main.be7089ff.js"
+  },
+  {
+    "revision": "6956dbda0015b26b8daac462ec922ef5",
+    "url": "/ACH_Task_Short_Psychedelic5/static/media/psych_logo.6956dbda.jpg"
+  },
+  {
+    "revision": "d9fbf22f557b87c921a73ef996721520",
+    "url": "/ACH_Task_Short_Psychedelic5/static/media/rating_keydown_1.d9fbf22f.png"
+  },
+  {
+    "revision": "6a659dd41fa6c15739f611662bf6d8d1",
+    "url": "/ACH_Task_Short_Psychedelic5/static/media/rating_keydown_2.6a659dd4.png"
+  },
+  {
+    "revision": "d955b91a2b4ba7dca40ee5ccb82d995b",
+    "url": "/ACH_Task_Short_Psychedelic5/static/media/rating_keydown_3.d955b91a.png"
+  },
+  {
+    "revision": "c8b7c6860406752a1915e08a432cb320",
+    "url": "/ACH_Task_Short_Psychedelic5/static/media/rating_keydown_4.c8b7c686.png"
+  },
+  {
+    "revision": "34958048ce563b1140c19e19f6e72410",
+    "url": "/ACH_Task_Short_Psychedelic5/static/media/rating_keydown_5.34958048.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/ACH_Task_Short_Psychedelic5/precache-manifest.fcf0ae6c0b72000de69757c6e44bd2a1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ACH_Task_Short_Psychedelic5/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
